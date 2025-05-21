769bb9f7ef47efb0fafd4e0c983e4efd3a05e89b
rodolfograve <rodolfograve@gmail.com>
2025-05-21 09:51:49 +0100

    Session 104

---
+++ TownleyPlayground/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Enter a number: ");
+var nStr = Console.ReadLine() ?? "";
+
+var n = long.Parse(nStr);
+
+long i = 2;
+while (i < n / 2)
+{
+    if (n % i == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{n} is not prime because it's divisible, at least, by {i}");
+        break;
+    }
+
+    if (i % 100000 == 0)
+    {
+        Console.Write($"{i}, ");
+    }
+
+    i++;
+}
+
+if (n % i != 0)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{n} is prime");
+}
