10e5020c8c06e76fb632a46690a3fbb1b33db37d
rodolfograve <rodolfograve@gmail.com>
2025-11-12 12:55:53 +0000

    Improved CardGame implementation and added Weather API

---
+++ UI/CardGame/GameCard.cs
@@ -0,0 +1,19 @@
+﻿namespace UI;
+
+public class GameCard
+{
+    public GameCard(string name, string description, string backgroundColor, string foregroundColor, string borderColor)
+    {
+        Name = name;
+        Description = description;
+        BackgroundColor = backgroundColor;
+        ForegroundColor = foregroundColor;
+        BorderColor = borderColor;
+    }
+
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string BackgroundColor { get; set; }
+    public string ForegroundColor { get; set; }
+    public string BorderColor { get; set; }
+}
