7d13d585354251a6b0c093a6f67954021741c9df
rodolfograve <rodolfograve@gmail.com>
2025-12-03 12:44:15 +0000

    Added a ConsoleUI playground

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿List<int> v = [10, 20, 30, 40, 50];
+ConsoleKeyInfo key;
+int i = 0;
+do
+{
+    Console.WriteLine($"The current number is '{v[i]}'. Press ESC to exit or any other key to get the next number.");
+    i = (i + 1) % v.Count;
+
+    key = Console.ReadKey();
+} while (key.Key != ConsoleKey.Escape);
+
+
